986e0d10fd728374c86a75c201e2646a42ecd92f
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-12-28 18:50:44 +0530

    new

--- extract_text.ps1
+++
@@ -1,7 +0,0 @@
-$path = "temp_cv\extracted\word\document.xml"
-$xml = [xml](Get-Content $path)
-$ns = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
-$ns.AddNamespace("w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main")
-$nodes = $xml.SelectNodes("//w:t", $ns)
-$text = $nodes | ForEach-Object { $_.InnerText }
-$text -join " " | Out-File -FilePath "cv_text.txt" -Encoding utf8
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/RishavMishraRM/RishavMishraRM.github.io
-
-go 1.21
